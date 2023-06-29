178ffaa9e84a65d9f5d440b80ad32f24ef4bc8a6
Alexey <komkov08@gmail.com>
2023-06-29 11:35:13 +0300

    Добавлено получение цены на покупкупо стакану и расчет количества лотов которое можно купить

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ with Client(token=creds.token) as client:
         d = v.units + v.nano / 1e9
         return d
 
+    f = open('turnover.txt')
+    turnover = int(f.read())
+    while turnover < 6000000:
+        r = client.market_data.get_last_prices(
+            figi=["BBG333333333"])
+        price_by = client.market_data.get_order_book(
+            figi="BBG333333333",
+            instrument_id='figi',
+            depth=4
+        ).bids[0].price
+        quantity = int(
+            (client.operations.get_withdraw_limits(account_id=creds.account_id).money[1].units // cast_money(
+            price_by)) - 1)
+
+
+
+
+
+
+
+
+
+
+
